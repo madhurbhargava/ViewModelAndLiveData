0131832bc5c22cb5224c47239e9833d9aa4ae72a
Madhur Bhargava <madhur.bhargava333@gmail.com>
2018-06-17 01:42:27 +0200

    Added LiveData

--- app/src/main/java/com/example/madhurbhargava/viewmodelandlivedata/LiveDataTimerViewModel.java
+++ app/src/main/java/com/example/madhurbhargava/viewmodelandlivedata/LiveDataTimerViewModel.java
@@ -1,6 +1,34 @@
 package com.example.madhurbhargava.viewmodelandlivedata;
 
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.os.SystemClock;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class LiveDataTimerViewModel extends ViewModel {
+
+    private final long ONE_SECOND = 1000;
+    private final long mInitialTime;
+
+    public MutableLiveData<Long> getElapsedTime() {
+        return elapsedTime;
+    }
+
+    private MutableLiveData<Long> elapsedTime = new MutableLiveData<>();
+
+    public LiveDataTimerViewModel() {
+        mInitialTime = SystemClock.elapsedRealtime();
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+                                      @Override
+                                      public void run() {
+
+                                          final long newValue = (SystemClock.elapsedRealtime() - mInitialTime) / 1000;
+                                          elapsedTime.postValue(newValue);
+
+                                      }
+                                  }, ONE_SECOND, ONE_SECOND);
+    }
 }
--- app/src/main/java/com/example/madhurbhargava/viewmodelandlivedata/MainActivity.java
+++ app/src/main/java/com/example/madhurbhargava/viewmodelandlivedata/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.madhurbhargava.viewmodelandlivedata;
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.SystemClock;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
@@ -12,11 +15,24 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LiveDataTimerViewModel viewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewModel = ViewModelProviders.of(this).get(LiveDataTimerViewModel.class);
+        subscribe();
+    }
 
-
+    private void subscribe() {
+        final Observer<Long> elapsedTimeObserver = new Observer<Long>() {
+            @Override
+            public void onChanged(@Nullable Long aLong) {
+                String newText = MainActivity.this.getResources().getString(R.string.seconds, aLong);
+                ((TextView) findViewById(R.id.textview_timer)).setText(newText);
+            }
+        };
+        viewModel.getElapsedTime().observe(this, elapsedTimeObserver);
     }
 }
